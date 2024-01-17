c360a81db80c16818dc2b3015f52d135f6593b54
Daniel Allison <hello@danielallison.dev>
2024-01-16 19:59:52 -0500

    feat: Add security rule in network-nsg1

--- automation/terraform/lab02s1/network-main.tf
+++ automation/terraform/lab02s1/network-main.tf
@@ -9,7 +9,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.87.0"
     }
   }
@@ -31,6 +31,18 @@ resource "azurerm_network_security_group" "network-nsg1" {
   name                = "network-nsg1"
   location            = azurerm_resource_group.network-rg.location
   resource_group_name = azurerm_resource_group.network-rg.name
+
+  security_rule {
+    name                       = "rule1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "network-vnet" {
@@ -41,13 +53,13 @@ resource "azurerm_virtual_network" "network-vnet" {
 }
 
 resource "azurerm_subnet" "network-subnet1" {
-  name = "network-subnet1"
-  resource_group_name = azurerm_resource_group.network-rg.name
+  name                 = "network-subnet1"
+  resource_group_name  = azurerm_resource_group.network-rg.name
   virtual_network_name = azurerm_virtual_network.network-vnet.name
-  address_prefixes = [ "10.0.0.0/24" ]
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
-resource "azurerm_subnet_network_security_group_association" "example" {
+resource "azurerm_subnet_network_security_group_association" "network-snsga1" {
   subnet_id                 = azurerm_subnet.network-subnet1.id
   network_security_group_id = azurerm_network_security_group.network-nsg1.id
 }
