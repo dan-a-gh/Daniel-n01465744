f94b296f4ac8dd052ffb2a22182cdb00beb18b5f
Daniel Allison <hello@danielallison.dev>
2024-01-18 18:40:21 -0500

    feat: Add network 2

--- automation/terraform/lab02s1/network-main.tf
+++ automation/terraform/lab02s1/network-main.tf
@@ -66,3 +66,48 @@ resource "azurerm_subnet_network_security_group_association" "network-snsga1" {
   subnet_id                 = azurerm_subnet.network-subnet1.id
   network_security_group_id = azurerm_network_security_group.network-nsg1.id
 }
+
+# Network 2
+# -----------------------------------------------------------------------------
+
+resource "azurerm_subnet" "network-subnet2" {
+  name                 = "network-subnet2"
+  resource_group_name  = azurerm_resource_group.network-rg.name
+  virtual_network_name = azurerm_virtual_network.network-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "network-nsg2" {
+  name                = "network-nsg2"
+  location            = azurerm_resource_group.network-rg.location
+  resource_group_name = azurerm_resource_group.network-rg.name
+
+  security_rule {
+    name                       = "rule1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "rule2"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "network-snsga2" {
+  subnet_id                 = azurerm_subnet.network-subnet2.id
+  network_security_group_id = azurerm_network_security_group.network-nsg2.id
+}
