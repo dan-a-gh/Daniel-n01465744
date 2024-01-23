17fe46f5e70c6bd507549a200a45a87d66e9106d
Daniel Allison <github@danielallison.dev>
2024-01-23 18:29:18 -0500

    style: Change resource names to use underscores instead of dashes

---
+++ automation/terraform/lab03s1/vmlinux-vars.tf
@@ -0,0 +1,56 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# vmlinux-vars.tf                                                             #
+# Created by Daniel Allison                                                   #
+# n01465744                                                                   #
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# VARIABLES
+# =============================================================================
+
+variable "linux_name" {
+  type    = string
+  default = "n01465744_u-vm1"
+}
+
+variable "linux_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "linux_admin_user" {
+  type    = string
+  default = "n01465744"
+}
+
+variable "public_key" {
+  type    = string
+  default = "/var/home/danielallison/.ssh/id_rsa.pub"
+}
+
+variable "linux_os_disk" {
+  type = object({
+    storage_account_type = string
+    disk_size            = number
+    caching              = string
+  })
+  default = {
+    storage_account_type = "Premium_LRS"
+    disk_size            = 32
+    caching              = "ReadWrite"
+  }
+}
+
+variable "linux_os" {
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+  default = {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "19.04"
+    version   = "latest"
+  }
+}
